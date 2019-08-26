b0d123a5664b46a675545fdabe06849e231daf36
Eric Cunningham <ericcunningham@erics-mbp.ams.softwaregrp.net>
2019-08-26 11:56:46 -0400

    bug-fix-1

--- src/routes/receipt.js
+++ src/routes/receipt.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
-const { secured } = require('./helpers');
+const {
+    secured
+} = require('./helpers');
 const navbarService = require('../services').NavbarService();
 const bookService = require('../services').BookService();
 
 const router = express.Router();
 
 router.get('/', secured, async function (req, res, next) {
-    const { id } = req.query;
+    const {
+        id
+    } = req.query;
     const flight = await bookService.getFlightById(req.user.name, id);
-    const { parting, returning, passengers } = flight;
+    const {
+        parting,
+        returning,
+        passengers
+    } = flight;
     const price = (parting.price + returning.price) * passengers;
     const vm = {
         nav: navbarService.getData(req),
@@ -22,9 +30,15 @@ router.get('/', secured, async function (req, res, next) {
             parting,
             returning
         },
-        totals: { price, passengers }
+        totals: {
+            price,
+            passengers
+        }
     }
     res.render('receipt', vm);
 });
 
 module.exports = router;
+
+
+//hi
